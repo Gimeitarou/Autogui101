dfa0ed3f4c33d2ed7e0cef255b8501c3923374e9
Gimeitarou <300kyen@gmail.com>
2024-12-14 18:37:07 +0900

    Create autogui-102.py

---
+++ autogui-102.py
@@ -0,0 +1,41 @@
+from sys import exit
+import pyautogui as pau
+import pyperclip
+import tkinter as tk
+import tkinter.messagebox as messagebox
+import time
+import os
+
+tk.Tk().withdraw()
+TorF = messagebox.askokcancel('<Consensus>', 'This code will automatically download a picture, and set the picture as your wallpaper.\nDo you accept that?')
+if TorF == False:
+    exit()
+
+DownloadCmd = 'powershell Invoke-WebRequest -Uri "https://www.python.jp/pages/python_logo2.png" -OutFile "~\Downloads\TheHopeOne.png"'
+
+OpenCmd = 'C:/Users/%username%/Downloads/TheHopeOne.png'
+
+#download a pic on DL-dir
+pyperclip.copy(DownloadCmd)
+pau.hotkey('win','r')
+pau.hotkey('ctrl','v')
+pau.press('enter')
+time.sleep(1)
+pau.hotkey('win','down')
+time.sleep(2)
+
+#open the pic
+pyperclip.copy(OpenCmd)
+pau.hotkey('win','r')
+pau.hotkey('ctrl','v')
+pau.press('enter')
+time.sleep(1)
+
+#set it as wallpaper
+pau.hotkey('ctrl','b')
+time.sleep(0.5)
+
+#close the pic
+pau.hotkey('alt','f4')
+
+os.remove('Downloads/TheHopeOne.png')
