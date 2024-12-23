7e414d0178c987015db0d472d1ee109c5ac96d6d
Gimeitarou <300kyen@gmail.com>
2024-12-23 18:30:22 +0900

    k

--- autogui-101-Agreement.py
+++ autogui-101-Agreement.py
@@ -11,8 +11,8 @@ TorF = messagebox.askokcancel('<Agreement>', 'This code will automatically downl
 if TorF == False:
     exit()
 
-DownloadCmd = 'powershell Invoke-WebRequest -Uri "https://www.python.jp/pages/python_logo2.png" -OutFile "~/Downloads/TheHopeOne.png"'
-OpenCmd = 'C:/Users/%username%/Downloads/TheHopeOne.png'
+DownloadCmd = 'powershell Invoke-WebRequest -Uri gahag.net/img/201608/26s/gahag-0119154009-1.jpg -OutFile Downloads/Lake.jpg'
+OpenCmd = 'C:/Users/%username%/Downloads/Lake.jpg'
 
 #download a pic on DL-dir
 pyperclip.copy(DownloadCmd)
@@ -37,4 +37,4 @@ time.sleep(2)
 #close the pic
 pau.hotkey('alt','f4')
 
-os.remove('Downloads/TheHopeOne.png')
+os.remove('Downloads/Lake.png')
