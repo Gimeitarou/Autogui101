e2a82949c471d517180ffec6d03d79a77ec2e52e
Gimeitarou <300kyen@gmail.com>
2025-05-04 13:33:53 +0900

    Def ammendment

--- autogui-101.py
+++ autogui-101.py
@@ -3,30 +3,37 @@ import pyperclip
 import time
 import os
 
-DownloadCmd = 'powershell Invoke-WebRequest -Uri the-public-domain-review.imgix.net/collections/campi-phlegraei/campi-phlegraei-9.jpg -OutFile Downloads/Lake.jpg'
-OpenCmd = 'C:/Users/%username%/Downloads/Lake.jpg'
+def autogui_101(Pic_URL):
 
-#download a pic on DL-dir
-pyperclip.copy(DownloadCmd)
-pau.hotkey('win','r')
-time.sleep(1)
-pau.hotkey('ctrl','v')
-pau.press('enter')
-time.sleep(10)
+    #cmddef    
+    DownloadCmd = f'powershell Invoke-WebRequest -Uri {Pic_URL} -OutFile Downloads/Pic.jpg'
+    OpenCmd = 'C:/Users/%username%/Downloads/Pic.jpg'
 
-#open the pic
-pyperclip.copy(OpenCmd)
-pau.hotkey('win','r')
-time.sleep(1)
-pau.hotkey('ctrl','v')
-pau.press('enter')
-time.sleep(2.5)
+    #download a pic on DL-dir
+    pyperclip.copy(DownloadCmd)
+    pau.hotkey('win','r')
+    time.sleep(1)
+    pau.hotkey('ctrl','v')
+    pau.press('enter')
+    time.sleep(10)
 
-#set it as wallpaper
-pau.hotkey('ctrl','b')
-time.sleep(2)
+    #open the pic
+    pyperclip.copy(OpenCmd)
+    pau.hotkey('win','r')
+    time.sleep(1)
+    pau.hotkey('ctrl','v')
+    pau.press('enter')
+    time.sleep(2.5)
 
-#close the pic
-pau.hotkey('alt','f4')
+    #set it as wallpaper
+    pau.hotkey('ctrl','b')
+    time.sleep(2)
 
-os.remove('Downloads/Lake.jpg')
+    #close the pic
+    pau.hotkey('alt','f4')
+
+    return 0
+
+ExURL_Lake = 'the-public-domain-review.imgix.net/collections/campi-phlegraei/campi-phlegraei-9.jpg'
+
+autogui_101(ExURL_Lake)
